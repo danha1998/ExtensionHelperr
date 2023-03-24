715fb776e2ed4310d5045e4bda91e4dbb27e6ce6
danhv <danhv@riki.edu.vn>
2023-03-24 23:04:00 +0700

    update

--- Sources/ExtensionHelperr/ExtensionHelperr.swift
+++ Sources/ExtensionHelperr/ExtensionHelperr.swift
@@ -8,7 +8,7 @@ public struct ExtensionHelperr: View {
     
     public var body: some View {
         VStack(spacing: 15) {
-            Image("imges_one")
+            Image(packageResource: "imges_one", ofType: "png")
                 .resizable()
                 .frame(width: 250, height: 250)
             Text("This is my framework").font(.system(size: 20, weight: .bold, design: .default)).fixedSize(horizontal: false, vertical: true)
