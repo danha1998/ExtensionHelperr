c28649cfc930a637f6752eca46163321729d0c2e
danhv <danhv@riki.edu.vn>
2023-03-26 23:10:55 +0700

    update

--- Package.swift
+++ Package.swift
@@ -20,17 +20,19 @@ let package = Package(
             url: "https://github.com/firebase/firebase-ios-sdk.git",
             .upToNextMajor(from: "10.4.0")
           ),
-         .package(url: "https://github.com/danha1998/ExtensionHelperr", from: "1.1.1"),
+         .package(url: "https://github.com/danha1998/ExtensionHelperr", from: "1.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ExtensionHelperr",
-            dependencies: [.product(name: "FirebaseRemoteConfig", package: "Firebase")],
+            dependencies: [.product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk")],
             resources: [.process("Resources")]),
         .testTarget(
             name: "ExtensionHelperrTests",
             dependencies: ["ExtensionHelperr"]),
     ]
 )
+
+//.product(name: "FirebaseRemoteConfig", package: "Firebase")
