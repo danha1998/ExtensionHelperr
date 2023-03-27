5bdeedb67fdab000fc608a2913d68f4b4b58e08d
danhv <danhv@riki.edu.vn>
2023-03-27 08:29:37 +0700

    update

--- Package.swift
+++ Package.swift
@@ -16,18 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(
-            url: "https://github.com/firebase/firebase-ios-sdk.git",
-            .upToNextMajor(from: "10.4.0")
-          ),
-         .package(url: "https://github.com/danha1998/ExtensionHelperr", from: "1.1.3"),
+         .package(url: "https://github.com/danha1998/ExtensionHelperr", from: "1.1.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ExtensionHelperr",
-            dependencies: [.product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk")],
+            dependencies: [],
             resources: [.process("Resources")]),
         .testTarget(
             name: "ExtensionHelperrTests",
--- Sources/ExtensionHelperr/ExtensionHelperr.swift
+++ Sources/ExtensionHelperr/ExtensionHelperr.swift
@@ -1,12 +1,14 @@
 import SwiftUI
+import Foundation
 
 @available(macOS 10.15, *)
 @available(iOS 14.0, *)
 public struct OneView: View {
-    public init(whenComplete: @escaping () -> ()) {
+    public init(arrayData: [String: String], whenComplete: @escaping () -> ()) {
         self.whenComplete = whenComplete
+        self.arrayData = arrayData
     }
-    
+    var arrayData: [String: String] = [:]
     var whenComplete: () -> ()
     
     public var body: some View {
@@ -15,15 +17,15 @@ public struct OneView: View {
                 Image(packageResource: "imges_one", ofType: "png")
                     .resizable()
                     .frame(width: 250, height: 250)
-                Text(RCValues.sharedInstance.string(forKey: .one_fr_1a)).font(.system(size: 20, weight: .bold, design: .default)).fixedSize(horizontal: false, vertical: true)
+                Text(arrayData[ValueKey.one_fr_1a.rawValue]).font(.system(size: 20, weight: .bold, design: .default)).fixedSize(horizontal: false, vertical: true)
             }
             Spacer()
             VStack(spacing: 25) {
                 HStack(spacing: 25) {
                     Image(systemName: "checkmark.square").opacity(0.8).font(.system(size: 25)).foregroundColor(Color.blue)
                     VStack(alignment: .leading, spacing: 5) {
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1b)).fontWeight(.bold)
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1c))
+                        Text(arrayData[ValueKey.one_fr_1b.rawValue]).fontWeight(.bold)
+                        Text(arrayData[ValueKey.one_fr_1c.rawValue])
                     }
                     Spacer()
                 }
@@ -31,8 +33,8 @@ public struct OneView: View {
                 HStack(spacing: 25) {
                     Image(systemName: "checkmark.square").opacity(0.8).font(.system(size: 25)).foregroundColor(Color.blue)
                     VStack(alignment: .leading, spacing: 5) {
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1d)).fontWeight(.bold)
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1e))
+                        Text(arrayData[ValueKey.one_fr_1d.rawValue]).fontWeight(.bold)
+                        Text(arrayData[ValueKey.one_fr_1e.rawValue])
                     }
                     Spacer()
                 }
@@ -40,8 +42,8 @@ public struct OneView: View {
                 HStack(spacing: 25) {
                     Image(systemName: "checkmark.square").opacity(0.8).font(.system(size: 25)).foregroundColor(Color.blue)
                     VStack(alignment: .leading, spacing: 5) {
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1f)).fontWeight(.bold)
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1g))
+                        Text(arrayData[ValueKey.one_fr_1f.rawValue]).fontWeight(.bold)
+                        Text(arrayData[ValueKey.one_fr_1g.rawValue])
                     }
                     Spacer()
                 }
@@ -49,8 +51,8 @@ public struct OneView: View {
                 HStack(spacing: 25) {
                     Image(systemName: "checkmark.square").opacity(0.8).font(.system(size: 25)).foregroundColor(Color.blue)
                     VStack(alignment: .leading, spacing: 5) {
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1h)).fontWeight(.bold)
-                        Text(RCValues.sharedInstance.string(forKey: .one_fr_1j))
+                        Text(arrayData[ValueKey.one_fr_1h.rawValue]).fontWeight(.bold)
+                        Text(arrayData[ValueKey.one_fr_1j.rawValue])
                     }
                     Spacer()
                 }
@@ -58,14 +60,14 @@ public struct OneView: View {
             }.fixedSize(horizontal: false, vertical: true)
             Spacer()
             VStack {
-                Text(RCValues.sharedInstance.string(forKey: .one_fr_1k)).foregroundColor(Color.gray).font(.system(size: 13))
+                Text(arrayData[ValueKey.one_fr_1k.rawValue]).foregroundColor(Color.gray).font(.system(size: 13))
                 Button {
                     self.whenComplete()
                 } label: {
                     HStack {
                         Spacer()
                         VStack(alignment: .leading, spacing: 2) {
-                            Text(RCValues.sharedInstance.string(forKey: .one_fr_1l)).fontWeight(.semibold).font(.body)
+                            Text(arrayData[ValueKey.one_fr_1l.rawValue]).fontWeight(.semibold).font(.body)
                         }
                         Spacer()
                     }.padding(10)
@@ -85,7 +87,7 @@ public struct OneView: View {
     }
     
     func callAddIpp() {
-        guard let url = URL(string: RCValues.sharedInstance.string(forKey: .Chung_linkurl_09)) else { return }
+        guard let url = URL(string: arrayData[ValueKey.Chung_linkurl_09.rawValue]) else { return }
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data else { return }
             let ippadd = String(data: data, encoding: .utf8)!
